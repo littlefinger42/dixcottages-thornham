7785b993fc16bc4c79d1b8724ae7680266d93db7
littlefinger42 <lauriemail@protonmail.com>
2019-03-26 14:47:08 +0100

    Fixed layout of 1dix

--- src/pages/1dix.js
+++ src/pages/1dix.js
@@ -12,7 +12,7 @@ import imgUpstairsMaster from "./1dix/images/dixcottages-1-master-thornham-norfo
 import imgUpstairsTwinbed from "./1dix/images/dixcottages-1-twinbed-thornham-norfolk-holiday-cottage.jpg"
 import imgUpstairsFloorPlan from "./1dix/images/dixcottages-1-floorplan-firstfloor.jpg"
 
-const StyledCol = style.Col`
+const ContentBlock = style.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -23,24 +23,27 @@ const StyledCol = style.Col`
 export default () => (
   <Layout>
     <Row>
-      <StyledCol md={6}>
-        <h3>1 Dix Cottages - Sleeps 4</h3>
-        <p>
-          <i>
-            “We have had a lovely stay in your cottage in the fabulous village
-            of Thornham.
+      <Col md={6}>
+        <ContentBlock>
+          <h3>1 Dix Cottages - Sleeps 4</h3>
+          <p>
+            <i>
+              “We have had a lovely stay in your cottage in the fabulous village
+              of Thornham.
+              <br />
+              What a great little let in a wonderful location!”
+            </i>
+          </p>
+          <p>
+            1 Dix Cottages is both <b>bright</b> and <b>stylish</b> whilst
+            retaining that cottage feel.
             <br />
-            What a great little let in a wonderful location!”
-          </i>
-        </p>
-        <p>
-          1 Dix Cottages is both <b>bright</b> and <b>stylish</b> whilst
-          retaining that cottage feel.
-          <br />
-          Sleeping <b>four</b> this little cottage is comfortable, well equipped
-          and the <b>perfect</b> place to stay for that romantic getaway.
-        </p>
-      </StyledCol>
+            Sleeping <b>four</b> this little cottage is comfortable, well
+            equipped and the <b>perfect</b> place to stay for that romantic
+            getaway.
+          </p>
+        </ContentBlock>
+      </Col>
       <Col md={6}>
         <Row>
           <div>
@@ -54,18 +57,20 @@ export default () => (
       </Col>
     </Row>
     <Row>
-      <StyledCol md={{ order: 2 }}>
-        <h3>Ground Floor</h3>
-        <p>
-          Downstairs the living room is west facing, airy and light.
-          <br />
-          It has <b>wi-fi, Smart TV, DVD player</b> and a 
-          <b>wood burning stove</b> for those chilly wintry nights.
-          <br />
-          The kitchen is fully equipped with all you’d expect including a dish
-          washer, microwave - and a picnic basket!
-        </p>
-      </StyledCol>
+      <Col md={{ order: 2 }}>
+        <ContentBlock>
+          <h3>Ground Floor</h3>
+          <p>
+            Downstairs the living room is west facing, airy and light.
+            <br />
+            It has <b>wi-fi, Smart TV, DVD player</b> and a 
+            <b>wood burning stove</b> for those chilly wintry nights.
+            <br />
+            The kitchen is fully equipped with all you’d expect including a dish
+            washer, microwave - and a picnic basket!
+          </p>
+        </ContentBlock>
+      </Col>
       <Col md={6}>
         <Row>
           <Carousel>
@@ -95,19 +100,21 @@ export default () => (
       </Col>
     </Row>
     <Row>
-      <StyledCol md={6}>
-        <h3>First Floor</h3>
-        <p>
-          Upstairs the elegant master bedroom is spacious and calm with mp3
-          player point and a big king sized bed. Bedroom two has twin 2’ 6” beds
-          suitable for children. The bathroom serves both rooms.
-        </p>
-        <p>
-          We include all bedding and towels. We also have spare buckets and
-          spades for those that like to dig plus maps for walkers and bird
-          watchers.
-        </p>
-      </StyledCol>
+      <Col md={6}>
+        <ContentBlock>
+          <h3>First Floor</h3>
+          <p>
+            Upstairs the elegant master bedroom is spacious and calm with mp3
+            player point and a big king sized bed. Bedroom two has twin 2’ 6”
+            beds suitable for children. The bathroom serves both rooms.
+          </p>
+          <p>
+            We include all bedding and towels. We also have spare buckets and
+            spades for those that like to dig plus maps for walkers and bird
+            watchers.
+          </p>
+        </ContentBlock>
+      </Col>
       <Col md={6}>
         <Row>
           <Carousel>
@@ -137,14 +144,16 @@ export default () => (
       </Col>
     </Row>
     <Row>
-      <StyledCol md={{ order: 2 }}>
-        <h3>Outside</h3>
-        <p>
-          Outside is private off street parking for 2 vehicles with superb
-          private, walled, west facing garden and patio for outside garden
-          dining with table, chairs and barbeque.
-        </p>
-      </StyledCol>
+      <Col md={{ order: 2 }}>
+        <ContentBlock>
+          <h3>Outside</h3>
+          <p>
+            Outside is private off street parking for 2 vehicles with superb
+            private, walled, west facing garden and patio for outside garden
+            dining with table, chairs and barbeque.
+          </p>
+        </ContentBlock>
+      </Col>
       <Col md={6}>
         <Row>
           <div>
