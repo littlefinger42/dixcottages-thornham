d0f144691456af85f5668753b62cb2e7552953f1
littlefinger42 <lauriemail@protonmail.com>
2019-03-27 19:35:52 +0100

    Updated styling of footer to improve colour contrasts

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,32 @@
 import React from "react"
+import style from "styled-components"
 
-const footerStyles = {
-	backgroundColor: '#746250',
-	textAlign: 'center',
-	color: 'white',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-	height: '48px',
-	width: '100%',
-	fontSize: '14px'
-}
+const FooterStyled = style.footer`
+	background-color: #746250;
+	text-align: center;
+	color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 48px;
+	width: 100%;
+	font-size: 14px;
+	a {
+		color: white;
+		text-decoration: underline;
+	}
+	a:hover {
+		color: white;
+		text-decoration: none;
+	}
+`
 
 export default props => (
-  <footer style={footerStyles}>
-    Copyright (c) 2019 Dix Cottages. All rights reserved. Contact the web developer at: <a href="https://l96.nl" title="l96.nl">l96.nl</a>
-  </footer>
+  <FooterStyled>
+    Copyright (c) 2019 Dix Cottages. All rights reserved. Contact the web
+    developer at:{" "}
+    <a href="https://l96.nl" title="l96.nl">
+      l96.nl
+    </a>
+  </FooterStyled>
 )
